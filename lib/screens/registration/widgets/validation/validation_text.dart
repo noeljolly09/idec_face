a5f056e4666334fffc550b028866acd5ea3a4e2b
151308 <151308@ust-global.com>
2022-05-26 14:46:20 +0530

    added validation text to bottom

--- lib/screens/registration/widgets/validation/validation_text.dart
+++ lib/screens/registration/widgets/validation/validation_text.dart
@@ -19,75 +19,79 @@ class ValidationText extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     double width10 = MediaQuery.of(context).size.width / 41.142;
-    return Row(
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Container(width: width10),
-        SvgPicture.asset(
-          assetName,
-          height: 25,
-        ),
-        Container(width: width10),
-        Container(
-          width: MediaQuery.of(context).size.width / 1.25,
-          padding: const EdgeInsets.only(top: 10, bottom: 10),
-          decoration: controllertext.isNotEmpty
-              ? BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.grey.shade400,
-                      width: 1.0,
-                    ),
-                  ),
-                )
-              : const BoxDecoration(
-                  border: Border(
-                    bottom: BorderSide(
-                      color: Colors.red,
-                      width: 1.0,
-                    ),
-                  ),
-                ),
-          child: Stack(children: [
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                controllertext.isNotEmpty
-                    ? Text(
-                        controllertext,
-                        style: TextStyle(
-                          color: AppConstants.customblack,
-                          fontSize: AppConstants.modaltextsize,
-                          fontStyle: FontStyle.normal,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      )
-                    : Text(
-                        titletext,
-                        style: TextStyle(
-                          fontSize: AppConstants.modaltextsize,
-                          color: AppConstants.customblack,
-                        ),
-                      ),
-                controllertext.isNotEmpty
-                    ? const Text("*")
-                    : const Text(
-                        "*",
-                        style: TextStyle(color: Colors.red),
-                      ),
-              ],
+        Row(
+          children: [
+            Container(width: width10),
+            SvgPicture.asset(
+              assetName,
+              height: 25,
             ),
+            Container(width: width10),
             Container(
-              margin: const EdgeInsets.only(right: 10),
-              child: Align(
-                  alignment: Alignment.bottomRight,
-                  child: controllertext.isNotEmpty
-                      ? null
+              width: MediaQuery.of(context).size.width / 1.25,
+              padding: const EdgeInsets.only(top: 10, bottom: 10),
+              decoration: controllertext.isNotEmpty
+                  ? BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.grey.shade400,
+                          width: 1.0,
+                        ),
+                      ),
+                    )
+                  : const BoxDecoration(
+                      border: Border(
+                        bottom: BorderSide(
+                          color: Colors.red,
+                          width: 1.0,
+                        ),
+                      ),
+                    ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  controllertext.isNotEmpty
+                      ? Text(
+                          controllertext,
+                          style: TextStyle(
+                            color: AppConstants.customblack,
+                            fontSize: AppConstants.modaltextsize,
+                            fontStyle: FontStyle.normal,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        )
+                      : Text(
+                          titletext,
+                          style: TextStyle(
+                            fontSize: AppConstants.modaltextsize,
+                            color: AppConstants.customblack,
+                          ),
+                        ),
+                  controllertext.isNotEmpty
+                      ? const Text("*")
                       : const Text(
-                          "Mandatory Field",
+                          "*",
                           style: TextStyle(color: Colors.red),
-                        )),
+                        ),
+                ],
+              ),
             ),
-          ]),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.only(left: 10),
+          child: Align(
+              alignment: Alignment.bottomRight,
+              child: controllertext.isNotEmpty
+                  ? null
+                  : const Text(
+                      "Mandatory Field",
+                      style: TextStyle(color: Colors.red),
+                    )),
         ),
       ],
     );
