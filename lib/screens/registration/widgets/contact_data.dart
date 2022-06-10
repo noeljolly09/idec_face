a4ee744744a302182806dcd88dad8736b593d10d
noeljolly09 <noel.jmail09@gmail.com>
2022-06-10 10:33:49 +0530

    .

--- lib/screens/registration/widgets/contact_data.dart
+++ lib/screens/registration/widgets/contact_data.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:country_list_pick/country_list_pick.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+
 import 'package:flutter_svg/svg.dart';
 
 import 'package:idec_face/custom_widgets/country_code_picker.dart';
