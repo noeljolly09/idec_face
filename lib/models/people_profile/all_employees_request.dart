b805338600c747df03c547be90ed37606550d098
noeljolly09 <62985022+noeljolly09@users.noreply.github.com>
2022-07-26 17:14:49 +0530

    Update all_employees_request.dart

--- lib/models/people_profile/all_employees_request.dart
+++ lib/models/people_profile/all_employees_request.dart
@@ -11,22 +11,30 @@ class EmployeeRequest {
     this.tabType,
     this.roleId,
     this.tradeId,
+    this.searchKey,
+    this.status,
   });
 
   String? tabType;
   String? roleId;
   String? tradeId;
+  String? searchKey;
+  bool? status;
 
   factory EmployeeRequest.fromJson(Map<String, dynamic> json) =>
       EmployeeRequest(
         tabType: json["tabType"],
         roleId: json["roleId"],
         tradeId: json["tradeId"],
+        searchKey: json["searchKey"],
+        status: json["status"],
       );
 
   Map<String, dynamic> toJson() => {
         "tabType": tabType,
         "roleId": roleId,
         "tradeId": tradeId,
+        "searchKey": searchKey,
+        "status": status,
       };
 }
