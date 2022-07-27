c683f1f527a54c08859e32da872c15abcc49fe5f
noeljolly09 <62985022+noeljolly09@users.noreply.github.com>
2022-07-27 18:23:20 +0530

    .

--- lib/screens/people_profile/enrolled_page.dart
+++ lib/screens/people_profile/enrolled_page.dart
@@ -6,7 +6,6 @@ import 'package:idec_face/constants.dart';
 import 'package:idec_face/custom_widgets/custom_appbar.dart';
 import 'package:idec_face/custom_widgets/search_bar.dart';
 import 'package:idec_face/dialogs/info_dialog/dialog_with_timer.dart';
-import 'package:idec_face/dialogs/profile_dialog.dart';
 import 'package:idec_face/models/people_profile/all_employees_request.dart';
 import 'package:idec_face/models/people_profile/all_employees_response.dart';
 import 'package:idec_face/network/core/service_constants/service_constants.dart';
